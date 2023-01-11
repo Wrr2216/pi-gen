65e836c170569581d62d58b60867210c27a7add9
Logan Miller <logan@millercyber.tech>
2023-01-10 20:39:25 -0600

    Update 02-run.sh

--- stage2/03-set-timezone/02-run.sh
+++ stage2/03-set-timezone/02-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "${TIMEZONE_DEFAULT}" > "${ROOTFS_DIR}/etc/timezone"
+echo "America/Chicago" > "${ROOTFS_DIR}/etc/timezone"
 rm "${ROOTFS_DIR}/etc/localtime"
 
 on_chroot << EOF
